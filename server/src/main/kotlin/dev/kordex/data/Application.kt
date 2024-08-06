db0e8b4a7bf23dbd11b0dd33959243a661dd0621
Gareth Coles <gdude2002@gmail.com>
2024-08-06 10:51:01 +0100

    Migrate, switch to info logging, update licence header

--- server/src/main/kotlin/dev/kordex/data/Application.kt
+++ server/src/main/kotlin/dev/kordex/data/Application.kt
@@ -2,12 +2,12 @@
  * Copyrighted (Kord Extensions, 2024). Licensed under the EUPL-1.2
  * with the specific provision (EUPL articles 14 & 15) that the
  * applicable law is the Irish law and the Jurisdiction Dublin.
- *
  * Any redistribution must include the specific provision above.
  */
 
 package dev.kordex.data
 
+import dev.kordex.data.db.Database
 import dev.kordex.data.plugins.configureHTTP
 import dev.kordex.data.plugins.configureRouting
 import dev.kordex.data.plugins.configureSerialization
@@ -16,6 +16,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
+	Database.db  // Causes migrations and such, when accessed for the first time
+
 	embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
 		.start(wait = true)
 }
